6ef61045c17306a49d709b762699fb7b17e110e8
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-09-21 16:31:16 +0300

    add todo list controller

---
+++ AspNetModelBindingSample.Api/Controllers/TodoListTaskController.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetModelBindingSample.Api.Controllers
+{
+  using Microsoft.AspNetCore.Mvc;
+
+  [Route("api/todo-list/{todoListId}")]
+  [Produces("application/json")]
+  public sealed class TodoListTaskController : ControllerBase
+  {
+  }
+}
